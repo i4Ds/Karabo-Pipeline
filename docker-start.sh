960ea3a7486ba35057ec789853a1a53431d20111
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-10-28 17:18:32 +0200

    fix git clone

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$(ls -A /home/jovyan/ska_pipeline)" ]; then
-   git clone https://github.com/i4Ds/SKA.git
+   git clone https://github.com/i4Ds/SKA.git /home/jovyan/ska_pipeline
 else
    echo "Pipeline code directory is not empty. There is code from a previous run. Skipping cloning of code."
 fi
