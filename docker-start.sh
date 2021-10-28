50982a474ea2064377d4f5bce9b2c1f6f1db2f70
Christoph Vögele <christoph.voegele@fhnw.ch>
2021-10-28 16:48:08 +0200

    rename folder check pipeline to ska_pipeline

--- docker-start.sh
+++ docker-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "$(ls -A /home/jovyan/pipeline)" ]; then
+if [ -z "$(ls -A /home/jovyan/ska_pipeline)" ]; then
    git clone https://github.com/i4Ds/SKA.git
 else
    echo "Pipeline code directory is not empty. There is code from a previous run. Skipping cloning of code."
