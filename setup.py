cd29a718cc6dcc01f04e63d2c152188008cc2126
Christoph Vögele <christoph.voegele@fhnw.ch>
2022-01-26 13:13:04 +0100

    add build scripts and conda-build.yml

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Karabo-Pipeline',
+    version='0.0.1',
+    packages=['karabo', 'karabo.data'],
+    url='https://github.com/i4Ds/SKA',
+    license='',
+    author='Simon Felix',
+    author_email='simon.felix@fhnw.ch',
+    description='A data-driven pipeline for Radio Astronomy from i4ds for the SKA Telescope',
+    include_package_data=True
+)
