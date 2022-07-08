fc30ce278babe22915b7bdae5b50d7a202a27b50
Filip Schramka <filip.schramka@fhnw.ch>
2022-07-08 16:25:22 +0200

    comment out test that is not rdy

--- karabo/test/test_pinocchio.py
+++ karabo/test/test_pinocchio.py
@@ -1,7 +1,12 @@
-from karabo.simulation.pinocchio import Pinocchio
+"""from karabo.simulation.pinocchio import Pinocchio
 
 def pinocchioFun():
     p = Pinocchio()
+    p.addRedShift("1.0")
+    p.printConfig()
+    p.printRedShiftRequest()
+    p.run()
+    p.save("/home/filip/pinocchiotest")
 
     #conf = p.getConfig()
     #conf["BoxSize"] = 400.0
@@ -11,7 +16,6 @@ def pinocchioFun():
 
     #p.plotMassfunction()
 
-    
-
-
 pinocchioFun()
+
+"""
