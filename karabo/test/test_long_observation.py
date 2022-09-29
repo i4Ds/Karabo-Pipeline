2219f31ac0367bd11af0bf7a41c665a92811c923
Rohit Sharma <rohitcbscient@gmail.com>
2022-09-29 22:04:56 +0200

    Adding script for simulating 1000 hours of observation for CSCS. (Minor beam fixes)

--- karabo/test/test_long_observation.py
+++ karabo/test/test_long_observation.py
@@ -27,9 +27,9 @@ class MyTestCase(unittest.TestCase):
         beam.fit_elements(tel, freq_hz=1.0e08, avg_frac_error=0.5)
 
     def test_katbeam(self):
-        beampixels = BeamPattern.get_meerkat_uhfbeam(f=800, pol="I", beamextent=40)
+        beampixels = BeamPattern.get_meerkat_uhfbeam(f=800, pol="I", beamextentx=40, beamextenty=40)
         BeamPattern.show_kat_beam(
-            beampixels, 40, 800, "I", path="./result/katbeam_beam.png"
+            beampixels[0], 40, 800, "I", path="./result/katbeam_beam.png"
         )
 
     def test_eidosbeam(self):
@@ -37,14 +37,13 @@ class MyTestCase(unittest.TestCase):
         dia = 10
         thres = 0
         ch = 0
-        B_ah = BeamPattern.get_eidos_holographic_beam(npix, ch, dia, thres, mode="AH")
+        B_ah = BeamPattern.get_eidos_holographic_beam(npix=npix, ch=ch, dia=dia, thres=thres, mode="AH")
         BeamPattern.show_eidos_beam(B_ah, path="./result/eidos_AH_beam.png")
-        B_em = BeamPattern.get_eidos_holographic_beam(npix, ch, dia, thres, mode="EM")
+        B_em = BeamPattern.get_eidos_holographic_beam(npix=npix, ch=ch, dia=dia, thres=thres, mode="EM")
         BeamPattern.show_eidos_beam(B_em, path="./result/eidos_EM_beam.png")
         BeamPattern.eidos_lineplot(
             B_ah, B_em, npix, path="./result/eidos_residual_beam.png"
         )
-
     def test_long_observations(self):
          number_of_days=3;hours_per_day=10
          combined_vis_filepath = './karabo/test/data/combined_vis.ms'
