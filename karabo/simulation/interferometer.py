7e5f0bde216cd3fe497b6883a59b0e9eaf22ba4e
Rohit Sharma <rohitcbscient@gmail.com>
2022-10-27 17:58:45 +0200

    Writing API for the long observations!

--- karabo/simulation/interferometer.py
+++ karabo/simulation/interferometer.py
@@ -2,14 +2,14 @@ import enum, os
 from typing import Dict, Union, Any
 
 import oskar
-
+import numpy as np
 import karabo.error
 from karabo.simulation.visibility import Visibility
 from karabo.simulation.observation import Observation
 from karabo.simulation.sky_model import SkyModel
 from karabo.simulation.telescope import Telescope
 from karabo.util.FileHandle import FileHandle
-
+from datetime import timedelta, datetime
 
 class CorrelationType(enum.Enum):
     """
@@ -176,3 +176,65 @@ class InterferometerSimulation:
             return "min"
         else:
             return str(uv_filter)
+
+def sky_tel_long(sky_data, telescope_path):
+        sky = SkyModel()
+        sky.add_point_sources(sky_data)
+        telescope = Telescope.read_OSKAR_tm_file(telescope_path)
+        return sky, telescope
+
+def create_vis_long(number_of_days:int, hours_per_day:int,
+                        sky_data, input_telescope, enable_array_beam:bool, xcstfile_path:str,ycstfile_path:str):
+        days = np.arange(1, number_of_days + 1);
+        visiblity_files = [0] * len(days);
+        ms_files = [0] * len(days);
+        i = 0
+        for day in days:
+            sky,telescope=sky_tel_long(sky_data, input_telescope.path)
+            # telescope.centre_longitude = 3
+            # Remove beam if already present
+            test = os.listdir(telescope.path)
+            for item in test:
+                if item.endswith(".bin"):
+                    os.remove(os.path.join(telescope.path, item))
+            if (enable_array_beam):
+                # ------------ X-coordinate
+                pb = BeamPattern(xcstfile_path)  # Instance of the Beam class
+                beam = pb.sim_beam(beam_method='Gaussian Beam')  # Computing beam
+                pb.save_meerkat_cst_file(beam[3])  # Saving the beam cst file
+                pb.fit_elements(telescope, freq_hz=1.e9, avg_frac_error=0.8, pol='X')  # Fitting the beam using cst file
+                # ------------ Y-coordinate
+                pb = BeamPattern(ycstfile_path)
+                pb.save_meerkat_cst_file(beam[4])
+                pb.fit_elements(telescope, freq_hz=1.e9, avg_frac_error=0.8, pol='Y')
+            print('Observing Day: ' + str(day))
+            # ------------- Simulation Begins
+            visiblity_files[i] = './karabo/test/data/beam_vis_' + str(day) + '.vis'
+            ms_files[i] = visiblity_files[i].split('.vis')[0] + '.ms'
+            os.system('rm -rf ' + visiblity_files[i]);
+            os.system('rm -rf ' + ms_files[i])
+            simulation = InterferometerSimulation(vis_path=visiblity_files[i],
+                                                  channel_bandwidth_hz=2e7,
+                                                  time_average_sec=1, noise_enable=False,
+                                                  noise_seed="time", noise_freq="Range", noise_rms="Range",
+                                                  noise_start_freq=1.e9,
+                                                  noise_inc_freq=1.e6,
+                                                  noise_number_freq=1,
+                                                  noise_rms_start=0.1,
+                                                  noise_rms_end=1,
+                                                  enable_numerical_beam=enable_array_beam,
+                                                  enable_array_beam=enable_array_beam)
+            # ------------- Design Observation
+            observation = Observation(mode='Tracking', phase_centre_ra_deg=20.0,
+                                      start_date_and_time=datetime(2000, 1, day, 11, 00, 00, 521489),
+                                      length=timedelta(hours=hours_per_day, minutes=0, seconds=0, milliseconds=0),
+                                      phase_centre_dec_deg=-30.0,
+                                      number_of_time_steps=1,
+                                      start_frequency_hz=1.e9,
+                                      frequency_increment_hz=1e6,
+                                      number_of_channels=1, )
+            visibility = simulation.run_simulation(telescope, sky, observation)
+            visibility.write_to_file(ms_files[i])
+            i = i + 1
+        return visiblity_files
+
