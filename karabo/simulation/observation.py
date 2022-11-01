c9060b162bd22ab73087277dddbf2844e8167d19
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2022-11-01 14:36:23 +0100

    finished setup of ObservationLong

--- karabo/simulation/observation.py
+++ karabo/simulation/observation.py
@@ -1,4 +1,5 @@
 import datetime, os, sys
+from operator import mod
 import numpy as np
 from datetime import timedelta, datetime
 from copy import deepcopy
@@ -6,7 +7,7 @@ from typing import List
 from karabo.error import KaraboError
 
 from karabo.karabo_resource import KaraboResource
-from karabo.simulation.interferometer import InterferometerSimulation
+from karabo.simulation.interferometer import InterferometerSimulation # causes circular import with Observation
 from karabo.simulation.sky_model import SkyModel
 from karabo.simulation.telescope import Telescope
 from karabo.simulation.beam import BeamPattern
@@ -114,99 +115,52 @@ class Observation(KaraboResource):
         return [self.phase_centre_ra_deg, self.phase_centre_dec_deg]
 
 
-class ObservationLong(KaraboResource):
+class ObservationLong(Observation):
+    """
+    This class allows the use of several observations on different days over a certain period of time within one day.
+    If only ONE observation is desired, even if it takes a little longer, this is already possible using `Observation`.
+    This class extends `Observation` so its parameters (except `length`) are not discussed here.
+    `length` is little different, which describes the duration of ONE observation, whose maximum duration for `ObservationLong` is 24h.
+
+    :ivar number_of_days: Number of successive days to observe
+    """
+
     def __init__(
         self,
-        observation:Observation,
-        interferometer_simulation:InterferometerSimulation,
-        sky_model:SkyModel,
-        telescope:Telescope,
-        number_of_days:int,
-        enable_array_beam:bool=None,
-        xcstfile_path:str=None,
-        ycstfile_path:str=None,
-        beam_method:str='Gaussian Beam',
-        avg_frac_error:float=.8,
+        mode:str='Tracking',
+        start_frequency_hz:float=0,
+        start_date_and_time:datetime=datetime.utcnow(),
+        length:timedelta=timedelta(hours=12),
+        number_of_channels:float=1,
+        frequency_increment_hz:float=0,
+        phase_centre_ra_deg:float=0,
+        phase_centre_dec_deg:float=0,
+        number_of_time_steps:float=1,
+        number_of_days:int=None,
     ) -> None:
 
-        self.observation: Observation = observation
-        self.interferometer_simulation : InterferometerSimulation = interferometer_simulation
-        self.sky_model : SkyModel = sky_model
-        self.telescope : Telescope = telescope
+        super.__init__(
+            mode=mode,
+            start_frequency_hz=start_frequency_hz,
+            start_date_and_time=start_date_and_time,
+            length=length,
+            number_of_channels=number_of_channels,
+            frequency_increment_hz=frequency_increment_hz,
+            phase_centre_ra_deg=phase_centre_ra_deg,
+            phase_centre_dec_deg=phase_centre_dec_deg,
+            number_of_time_steps=number_of_time_steps,
+        )
         self.number_of_days : int = number_of_days
-        self.enable_array_beam : bool = enable_array_beam
-        self.xcstfile_path : str = xcstfile_path
-        self.ycstfile_path : str = ycstfile_path
-        self.beam_method : str = beam_method
-        self.avg_frac_error: float = avg_frac_error
+        self.__check_attrs()
 
-        self.sky_data = deepcopy(self.sky_model.sources)
-        self.vis_path = self.interferometer_simulation.vis_path
-        self.interferometer_simulation.enable_array_beam = self.enable_array_beam
-        self.interferometer_simulation.enable_numerical_beam = self.enable_array_beam
-
-        self.__check_input()
-
-    def __check_input(self) -> None:
-        if self.enable_array_beam and (self.xcstfile_path is None or self.ycstfile_path is None):
-            raise KaraboError(f'`enable_array_beam` is {self.enable_array_beam} \
-                but `xcstfile_path` and/or `ycstfile_path` are None!')
+    def __check_attrs(self) -> None:
         if not isinstance(self.number_of_days, int):
             raise KaraboError(f'`number_of_days` must be of type int but is of type {type(self.number_of_days)}!')
         if self.number_of_days <= 1:
             raise KaraboError(f'`number_of_days` must be >=2 but is {self.number_of_days}!')
+        if self.length > timedelta(hours=24):
+            raise KaraboError(f'`length` should be max 24 hours but is {self.length}!')
 
-    def create_vis_long(self) -> List[str]:
-        #days = np.arange(1, self.number_of_days + 1)
-        visiblity_files = [0] * self.number_of_days
-        ms_files = [0] * self.number_of_days
-        current_date = self.observation.start_date_and_time
-        tel_type = self.telescope.path.split('/')[-1].split('.tm')[0] # works as long as `read_OSKAR_tm_file` sets telescope.path
-        if os.path.exists(self.vis_path):
-            ans = input(f'{self.vis_path} already exists. Do you want to replace it? [y/N]')
-            if ans != 'y':
-                sys.exit(0)
-        for i in range(self.number_of_days):
-            sky = SkyModel(sources=deepcopy(self.sky_data)) # is deepcopy or copy needed?
-            telescope = Telescope.read_OSKAR_tm_file(self.telescope.path)
-            # telescope.centre_longitude = 3
-            # Remove beam if already present
-            test = os.listdir(telescope.path)
-            for item in test:
-                if item.endswith(".bin"):
-                    os.remove(os.path.join(telescope.path, item))
-            if self.enable_array_beam:
-                # ------------ X-coordinate
-                pb = BeamPattern(self.xcstfile_path)  # Instance of the Beam class
-                beam = pb.sim_beam(beam_method=self.beam_method)  # Computing beam
-                pb.save_cst_file(beam[3], telescope_type=tel_type)  # Saving the beam cst file
-                pb.fit_elements(
-                    telescope,
-                    freq_hz=self.observation.start_frequency_hz,
-                    avg_frac_error=self.avg_frac_error,
-                    pol='X',
-                )  # Fitting the beam using cst file
-                # ------------ Y-coordinate
-                pb = BeamPattern(self.ycstfile_path)
-                pb.save_cst_file(beam[4], telescope_type=tel_type)
-                pb.fit_elements(
-                    telescope,
-                    freq_hz=self.observation.start_frequency_hz,
-                    avg_frac_error=self.avg_frac_error,
-                    pol='Y',
-                )
-            print('Observing Day: ' + str(i) + ' the ' + str(current_date))
-            # ------------- Simulation Begins
-            visiblity_files[i] = os.path.join(self.vis_path, 'beam_vis_' + str(i) + '.vis')
-            ms_files[i] = visiblity_files[i].split('.vis')[0] + '.ms'
-            os.system('rm -rf ' + visiblity_files[i])
-            os.system('rm -rf ' + ms_files[i])
-            simulation = deepcopy(self.interferometer_simulation)
-            simulation.vis_path = visiblity_files[i]
-            # ------------- Design Observation
-            observation = deepcopy(self.observation)
-            observation.start_date_and_time = current_date
-            visibility = simulation.run_simulation(telescope, sky, observation)
-            visibility.write_to_file(ms_files[i])
-            current_date + timedelta(days=1)
-        return visiblity_files
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value.upper()
+        self.__check_attrs()
