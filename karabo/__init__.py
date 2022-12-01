a2f131c44772f09dd702670e3e582b50ad02b82d
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2022-12-01 11:10:34 +0100

    added wsl shared library path to root __init__.py

--- karabo/__init__.py
+++ karabo/__init__.py
@@ -1,2 +1,12 @@
+# set rascil data directory environment variable (see https://ska-telescope.gitlab.io/external/rascil/RASCIL_install.html)
 from karabo.util.jupyter import set_rascil_data_directory_env
 set_rascil_data_directory_env()
+
+# set shared library if WSL to detect GPU drivers
+import os, platform
+if 'WSL' in platform.release():
+    wsl_ld_path = ':/usr/lib/wsl/lib'
+    if os.environ.get('LD_LIBRARY_PATH') is None:
+        os.environ['LD_LIBRARY_PATH'] = wsl_ld_path
+    else:
+        os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH']+wsl_ld_path
