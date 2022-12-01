12594deb49b89ea34ae950e16a24723a6824729a
Vincenzo Timmel <vincenzo.timmel@gmail.com>
2022-12-01 17:42:47 +0100

    Updated method to set LD_LIBRARY_PATH on wsl.

--- karabo/__init__.py
+++ karabo/__init__.py
@@ -1,12 +1,19 @@
-# set rascil data directory environment variable (see https://ska-telescope.gitlab.io/external/rascil/RASCIL_install.html)
-from karabo.util.jupyter import set_rascil_data_directory_env
-set_rascil_data_directory_env()
-
 # set shared library if WSL to detect GPU drivers
-import os, platform
-if 'WSL' in platform.release():
-    wsl_ld_path = ':/usr/lib/wsl/lib'
+import os, platform, sys
+
+if 'WSL' in platform.release() and 'wsl/lib' not in os.environ.get('LD_LIBRARY_PATH'):
+    wsl_ld_path = '/usr/lib/wsl/lib'
     if os.environ.get('LD_LIBRARY_PATH') is None:
         os.environ['LD_LIBRARY_PATH'] = wsl_ld_path
     else:
-        os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH']+wsl_ld_path
+        os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH']+':'+wsl_ld_path
+    # Restart Python Interpreter
+    # https://stackoverflow.com/questions/6543847/setting-ld-library-path-from-inside-python
+    os.execv(sys.argv[0], sys.argv)
+    
+    
+# set rascil data directory environment variable (see https://ska-telescope.gitlab.io/external/rascil/RASCIL_install.html)
+from karabo.util.jupyter import set_rascil_data_directory_env
+import sys
+
+set_rascil_data_directory_env()
