81e35984f0f811efcfb57a6588be3cc1fee939c1
Vincenzo Timmel <vincenzo.timmel@gmail.com>
2022-12-20 15:45:35 +0100

    Moved version to the top, added version to the setup file.

--- karabo/version.py
+++ karabo/version.py
@@ -1,11 +1,11 @@
 import os
-
+###
 __version__ = '0.11.1'
+###
+
+
 if os.getenv('BUILD_NIGHTLY', 'False') == 'True':
     __version__ = __version__ + '.nightly'
-    
-
-
 
 if __name__ == '__main__':
     import karabo
--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from distutils.core import setup
+from karabo.version import __version__
 # implicitly takes config from setup.cfg (used by conda build load_setup_py_data())
-setup()
+setup(
+    version=__version__,
+)
