6be7605b736a8d15807ab487ff8b0f8c1e3ed46a
Rohit Sharma <rohitcbscient@gmail.com>
2023-01-04 10:19:49 +0100

    Adding Gauss function to the branch

--- karabo/util/data_util.py
+++ karabo/util/data_util.py
@@ -48,6 +48,44 @@ def read_CSV_to_ndarray(file: str) -> np.ndarray:
     return np.array(sources, dtype=float)
 
 
+def Gauss(x, x0, y0, a,  sigma):
+    return y0 + a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
+
+def Voigt(x, x0, y0, a, sigma, gamma):
+    # sigma = alpha / np.sqrt(2 * np.log(2))
+    return y0 + a * np.real(wofz((x - x0 + 1j * gamma) / sigma / np.sqrt(2))) / sigma / np.sqrt(2 * np.pi)
+
+def get_spectral_sky_data(
+    ra: NDArray[np.float64],
+    dec: NDArray[np.float64],
+    freq0: NDArray[np.float64],
+    nfreq: int,
+) -> NDArray[np.float64]:
+    dfreq_arr = np.linspace(-0.1, 0.1, 100)
+    y_voigt = Voigt(dfreq_arr, 0, 0, 1, 0.01, 0.01)
+    y_gauss = Gauss(dfreq_arr, 0, 0, 1, 0.01)
+    dfreq_sample = dfreq_arr[::nfreq]
+    flux_sample = y_voigt[::nfreq]
+    freq_sample = freq0+dfreq_sample * freq0
+    sky_data = np.zeros((nfreq,12))
+    sky_data[:,0] = ra
+    sky_data[:,1] = dec
+    sky_data[:,2] = flux_sample
+    sky_data[:,6] = freq_sample
+    sky_data[:,7] = -200
+    return sky_data
+
+def resample_spectral_lines(
+    npoints: int,
+    dfreq: NDArray[np.float64],
+    spec_line: NDArray[np.float64],
+) -> Tuple[NDArray[np.float64], NDArray[np.float64]]:
+    m = int(len(dfreq) / npoints)
+    dfreq_sampled = dfreq[::m]
+    line_sampled = spec_line[::m]
+    return dfreq_sampled, line_sampled
+
+
 def full_setter(self, state):
     self.__dict__ = state
 
