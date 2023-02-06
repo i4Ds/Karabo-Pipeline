bebe6272855dfff68138a99a4df1632be010db4b
Vincenzo Timmel <vincenzo.timmel@gmal.com>
2023-02-06 16:01:40 +0100

    Fixed test pathing.

--- karabo/test/test_examples.py
+++ karabo/test/test_examples.py
@@ -6,8 +6,6 @@ class TestDocExamples(unittest.TestCase):
     def test_examples(self):
         """Test all examples in the documentation"""
         # get all example scripts
-        path = os.path.dirname(os.path.abspath(__file__))
-        os.chdir(path + "/../..")
         example_scripts = glob.glob("doc/src/examples/_example_scripts/*.py")
         # run all example scripts
         for example_script in example_scripts:
