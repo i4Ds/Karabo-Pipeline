dc23e18c678e8a9043a755d904339f5d4b201e87
Vincenzo Timmel <vincenzo.timmel@gmal.com>
2023-02-08 11:50:00 +0100

    Fixed tests, added test dependencies

--- karabo/test/test_file_handle.py
+++ karabo/test/test_file_handle.py
@@ -10,23 +10,16 @@ class TestFileHandle(unittest.TestCase):
         handle = FileHandle()
         path = handle.path
         self.assertTrue(os.path.exists(path))
-        del handle
-        self.assertTrue(not os.path.exists(path))
 
     def test_folder(self):
         handle = FileHandle(is_dir=True)
         path = handle.path
         self.assertTrue(os.path.exists(path))
-        del handle
-        self.assertTrue(not os.path.exists(path))
 
     def test_existing_file(self):
         handle = FileHandle(existing_file_path=f"{data_path}/detection.csv")
         path = handle.path
         self.assertTrue(os.path.exists(path))
-        del handle
-        self.assertTrue(not os.path.exists(path))
-        self.assertTrue(os.path.exists(f"{data_path}/detection.csv"))
 
     def test_existing_folder(self):
         handle = FileHandle(
@@ -35,5 +28,10 @@ class TestFileHandle(unittest.TestCase):
         path = handle.path
         self.assertTrue(os.path.exists(path))
         del handle
-        self.assertTrue(not os.path.exists(path))
-        self.assertTrue(os.path.exists(f"{data_path}/poisson_vis.ms"))
+        
+    def test_cleanup(self):
+        handle = FileHandle()
+        path = handle.path
+        self.assertTrue(os.path.exists(path))
+        handle.clean_up()
+        self.assertFalse(os.path.exists(path))
