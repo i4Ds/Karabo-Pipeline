4ad62063720035f2089b9cc59a1e5586995a6932
Vincenzo Timmel <vincenzo.timmel@gmal.com>
2023-03-17 14:55:10 +0100

    Removed try except.

--- karabo/util/gpu_util.py
+++ karabo/util/gpu_util.py
@@ -3,19 +3,16 @@ import os
 
 def is_cuda_available() -> bool:
     # Check available GPU by invoking nvidia-smi
-    try:
-        output = os.popen("nvidia-smi").read()
-        if "GPU" in output and "CUDA" in output:
-            return True
-        elif (
-            "nvidia-smi: not found" in output
-            or "NVIDIA-SMI has failed because it couldn't communicate" in output
-        ):
-            return False
-        else:
-            print("Unexpected output from nvidia-smi: ", output)
-            return False
-    except Exception:
+    output = os.popen("nvidia-smi").read()
+    if "GPU" in output and "CUDA" in output:
+        return True
+    elif (
+        "nvidia-smi: not found" in output
+        or "NVIDIA-SMI has failed because it couldn't communicate" in output
+    ):
+        return False
+    else:
+        print("Unexpected output from nvidia-smi: ", output)
         return False
 
 
