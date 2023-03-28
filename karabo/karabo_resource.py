621a8ed2e951460c2337f7dfe8f96122a16012ae
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2023-03-28 16:58:20 +0200

    mypy karabo-resource

--- karabo/karabo_resource.py
+++ karabo/karabo_resource.py
@@ -1,4 +1,5 @@
-from typing import Any
+from types import TracebackType
+from typing import Any, Optional
 
 import numpy as np
 
@@ -20,8 +21,13 @@ class KaraboResource:
 
 
 class NumpyAssertionsDisabled:
-    def __enter__(self):
+    def __enter__(self) -> None:
         self.old_settings = np.seterr(all="ignore")
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
         np.seterr(**self.old_settings)
