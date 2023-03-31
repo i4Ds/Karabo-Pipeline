673ccd88a4443ce132d74de9c0d1f9d22c263af2
Vincenzo Timmel <vincenzo.timmel@gmal.com>
2023-03-31 13:15:56 +0200

    Adds function get vram.

---
+++ karabo/test/test_utils.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+
+from karabo.util.gpu_util import get_gpu_memory, is_cuda_available
+
+RUN_GPU_TESTS = os.environ.get("RUN_GPU_TESTS", "false").lower() == "true"
+
+
+class TestGpuUtils(unittest.TestCase):
+    @unittest.skipIf(not RUN_GPU_TESTS, "GPU tests are disabled")
+    def test_get_gpu_memory(self):
+        memory = get_gpu_memory()
+        assert isinstance(memory, int)
+        assert memory > 0
+
+    def test_gpu_memory_error(self):
+        with self.assertRaises(RuntimeError):
+            get_gpu_memory()
+
+    def test_is_cuda_available(self):
+        assert isinstance(is_cuda_available(), bool)
+
+    @unittest.skipIf(not RUN_GPU_TESTS, "GPU tests are disabled")
+    def test_is_cuda_available_true(self):
+        assert is_cuda_available()
--- karabo/util/gpu_util.py
+++ karabo/util/gpu_util.py
@@ -18,5 +18,15 @@ def is_cuda_available() -> bool:
         return False
 
 
+def get_gpu_memory() -> int:
+    # Check available GPU by invoking nvidia-smi
+    # "2> /dev/null" surpresses stderr if command not found and returns ""
+    output = os.popen("nvidia-smi 2> /dev/null").read()
+    if "GPU" in output and "CUDA" in output:
+        return int(output.split("MiB")[1].split(" ")[-1])
+    else:
+        raise RuntimeError("Unexpected output from nvidia-smi:", f'"{output}"')
+
+
 if __name__ == "__main__":
     print(is_cuda_available())
