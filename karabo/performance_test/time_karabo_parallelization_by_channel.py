442c4c10b724ffc43203a072fa51aab5a20d6e51
Vincenzo Timmel <vincenzo.timmel@gmal.com>
2023-05-18 12:58:32 +0200

    Removed trailing white space

--- karabo/performance_test/time_karabo_parallelization_by_channel.py
+++ karabo/performance_test/time_karabo_parallelization_by_channel.py
@@ -45,7 +45,9 @@ def main(n_channels: int) -> None:
     time_taken = round((time.time() - start) / 60, 2)
     print("Time taken: (minutes)", time_taken)
 
-    with open(f"output_{str(get_number_of_nodes())}_nodes_{str(n_channels)}_channels.txt", "a") as file: # noqa: E501 
+    with open(
+        f"output_{str(get_number_of_nodes())}_nodes_{str(n_channels)}_channels.txt", "a"
+    ) as file:  # noqa: E501
         file.write(
             f"Number of channels: {str(n_channels)}. "
             f"Time taken: {str(time_taken)} min.\n"
