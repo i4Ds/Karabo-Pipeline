b57f7b400bcec1a8229a7474c9a1a3431c804a0f
Rohit Sharma <rohitcbscient@gmail.com>
2023-05-24 16:06:47 +0200

    RASCIL Workflow attempt-1

---
+++ karabo/test/test_rascil_workflow.py
@@ -0,0 +1,106 @@
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+import numpy as np
+from ska_sdp_datamodels.calibration import (
+    export_pointingtable_to_hdf5,
+    export_gaintable_to_hdf5,
+)
+from ska_sdp_datamodels.science_data_model.polarisation_model import PolarisationFrame
+from ska_sdp_datamodels.sky_model import SkyComponent, export_skycomponent_to_hdf5
+from ska_sdp_datamodels.visibility import export_visibility_to_hdf5, create_visibility
+from ska_sdp_datamodels.configuration import (
+    create_named_configuration,
+    select_configuration,
+)
+from ska_sdp_datamodels.sky_model.sky_model import SkyModel
+from ska_sdp_func_python.image.deconvolution import (
+    radler_deconvolve_list,
+    deconvolve_list,
+    deconvolve_cube,
+)
+
+from rascil.processing_components.imaging.primary_beams import (
+    create_pb,
+    create_low_test_beam,
+)
+
+from ska_sdp_func_python.imaging import (
+    invert_visibility,invert_ng,
+    predict_visibility,
+    normalise_sumwt,
+    taper_visibility_gaussian,
+    remove_sumwt,
+    sum_predict_results,
+    sum_invert_results,
+)
+
+from rascil.processing_components import create_test_image
+
+from ska_sdp_func_python.imaging import (
+    advise_wide_field,
+    create_image_from_visibility,
+)
+from ska_sdp_func_python.visibility import convert_visibility_stokesI_to_polframe
+from ska_sdp_func_python.calibration import multiply_gaintables
+
+from rascil.workflows.rsexecute.execution_support import rsexecute, get_dask_client
+
+
+results_dir = "/home/rohit/simulations/rascil_results/"
+# Construct LOW core configuration
+lowr3 = create_named_configuration("LOWBD2", rmax=750.0)
+# We create the visibility. This just makes the uvw, time, antenna1, antenna2,
+# weight columns in a table. We subsequently fill the visibility value in by
+# a predict step.
+times = np.zeros([1])
+frequency = np.array([1e8])
+channel_bandwidth = np.array([1e6])
+phasecentre = SkyCoord(
+    ra=+15.0 * u.deg, dec=-45.0 * u.deg, frame="icrs", equinox="J2000"
+)
+vt = create_visibility(
+    lowr3,
+    times,
+    frequency,
+    channel_bandwidth=channel_bandwidth,
+    weight=1.0,
+    phasecentre=phasecentre,
+    polarisation_frame=PolarisationFrame("stokesI"),
+)
+
+advice = advise_wide_field(
+    vt, guard_band_image=3.0, delA=0.1, oversampling_synthesised_beam=4.0
+)
+cellsize = advice["cellsize"]
+# Read the venerable test image, constructing a RASCIL Image
+m31image = create_test_image(
+    cellsize=cellsize, frequency=frequency, phasecentre=vt.phasecentre
+)
+
+model = create_image_from_visibility(vt, cellsize=cellsize, npixel=512)
+dirty, sumwt = invert_visibility(vt, model, context="2d")
+psf, sumwt = invert_ng(vt, model, context='2d', dopsf=True)
+
+#        model_list = [rsexecute.execute(create_image_from_visibility)
+#            (v, npixel=npixel, cellsize=cellsize, polarisation_frame=pol_frame)
+#            for v in vis_list]
+
+#        model_list = rsexecute.persist(model_list)
+#        dirty_list = invert_list_rsexecute_workflow(vis_list, template_model_imagelist=model_list, context='wstack',vis_slices=51)
+#        dirty_sumwt_list = rsexecute.compute(dirty_list, sync=True)
+#        dirty, sumwt = dirty_sumwt_list[centre]
+# Deconvolve using clean
+comp, residual = deconvolve_cube(
+    dirty,
+    psf,
+    niter=10000,
+    threshold=0.001,
+    fractional_threshold=0.001,
+    window_shape="quarter",
+    gain=0.7,
+    scales=[0, 3, 10, 30],
+)
+
+
+export_image_to_fits(dirty, "%s/imaging_dirty.fits" % (results_dir))
+export_image_to_fits(psf, "%s/imaging_psf.fits" % (results_dir))
