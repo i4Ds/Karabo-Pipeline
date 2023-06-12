03e590987e03c62a9c68e4f93e1a2a9cdabf2266
vtimmel <vtimmel@fhnw.ch>
2023-06-12 15:14:15 +0200

    Updated script to test reconstrunction to check vis combine of different sky chunks.

--- karabo/performance_test/time_karabo_reconstruction.py
+++ karabo/performance_test/time_karabo_reconstruction.py
@@ -3,18 +3,19 @@ from karabo.simulation.line_emission import karabo_reconstruction
 
 
 if __name__ == "__main__":
-    # Reating in sky model
+    print("Loading sky model")
     sky = SkyModel.get_BATTYE_sky()
     phase_center = [21.44213503, -30.70729488]
-    # Filtering sky model
-    sky.filter_by_radius_euclidean_flat_approximation(
+    print("Filtering sky model")
+    sky = sky.filter_by_radius_euclidean_flat_approximation(
         0,
         3,
         phase_center[0],
         phase_center[1],
     )
+    print("Reconstructing sky model")
     karabo_reconstruction(
-        "/scratch/snx3000/vtimmel/karabo_folder/test_recon", 
+        "/scratch/snx3000/vtimmel/karabo_folder/test_recon2/", 
         sky=sky, 
         pdf_plot=True,
         cut=3,
