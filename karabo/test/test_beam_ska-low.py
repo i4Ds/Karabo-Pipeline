c59c29675e7da432afc954e50edd4eab76f2e76a
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2023-06-12 10:57:59 +0200

    fixed dev-tools complaints

--- karabo/test/test_beam_ska-low.py
+++ karabo/test/test_beam_ska-low.py
@@ -1,21 +1,20 @@
+import glob
 import os
 import unittest
 from datetime import datetime, timedelta
-import glob
-import numpy as np
 
-from karabo.simulation.beam import BeamPattern
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.io import fits
+
+from karabo.imaging.imager import Imager
 from karabo.simulation.interferometer import InterferometerSimulation
 from karabo.simulation.observation import Observation
 from karabo.simulation.sky_model import SkyModel
 from karabo.simulation.telescope import Telescope
-from karabo.imaging.imager import Imager
-from astropy.io import fits
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 
-from karabo.test import data_path
+matplotlib.use("TkAgg")
 
 
 class MyTestCase(unittest.TestCase):
@@ -28,11 +27,10 @@ class MyTestCase(unittest.TestCase):
     def make_ska_low_telescope(self):
         telescope = Telescope.get_SKA1_LOW_Telescope()
         telescope1 = Telescope.get_OSKAR_Example_Telescope()
-        layout_path=telescope1.path+'/station000/layout.txt'
-        flist=sorted(glob.glob(telescope.path+'/station*'))
+        layout_path = telescope1.path + "/station000/layout.txt"
+        flist = sorted(glob.glob(telescope.path + "/station*"))
         for i in range(len(flist)):
-            os.system('cp -r '+layout_path+' '+flist[i])
-
+            os.system("cp -r " + layout_path + " " + flist[i])
 
     def test_beam(self):
         sky = SkyModel()
@@ -48,14 +46,14 @@ class MyTestCase(unittest.TestCase):
         # telescope.centre_longitude = 3
         enable_array_beam = True
         # Remove beam if already present
-        #------------- Simulation Begins
+        # ------------- Simulation Begins
         simulation = InterferometerSimulation(
             vis_path="./karabo/test/data/beam_vis.vis",
             channel_bandwidth_hz=2e7,
             time_average_sec=1,
             noise_enable=False,
             enable_numerical_beam=True,
-            station_type = 'Gaussian',
+            station_type="Gaussian",
             enable_array_beam=enable_array_beam,
         )
         observation = Observation(
@@ -70,20 +68,25 @@ class MyTestCase(unittest.TestCase):
             number_of_channels=1,
         )
         visibility = simulation.run_simulation(telescope, sky, observation)  # noqa
-        #visibility.write_to_file("./test/result/beam/beam_vis.ms")
+        # visibility.write_to_file("./test/result/beam/beam_vis.ms")
 
-        imager = Imager(visibility, imaging_npixel=4096,imaging_cellsize=50)
+        imager = Imager(visibility, imaging_npixel=4096, imaging_cellsize=50)
         dirty = imager.get_dirty_image()
-        dirty.write_to_file("./karabo/test/result/beam/ska_low_array_vis.fits",overwrite=True)
-        #dirty.plot(title='Flux Density (Jy)')
-        #os.system('mv ./karabo/test/result/beam/ska_low_array_vis.fits ./karabo/test/result/beam/ska_low_no_array_vis.fits')
-        aa=fits.open('./karabo/test/result/beam/ska_low_array_vis.fits')
-        bb=fits.open('./karabo/test/result/beam/ska_low_no_array_vis.fits')
-        print(np.nanmax(aa[0].data-bb[0].data),np.nanmax(aa[0].data),np.nanmax(bb[0].data))
-        diff=aa[0].data[0][0]-bb[0].data[0][0]
-        f,ax=plt.subplots(1,1)
-        ax.imshow(diff,aspect='auto',origin='lower',vmin=-1.e-1,vmax=1.e-1)
+        dirty.write_to_file(
+            "./karabo/test/result/beam/ska_low_array_vis.fits", overwrite=True
+        )
+        aa = fits.open("./karabo/test/result/beam/ska_low_array_vis.fits")
+        bb = fits.open("./karabo/test/result/beam/ska_low_no_array_vis.fits")
+        print(
+            np.nanmax(aa[0].data - bb[0].data),
+            np.nanmax(aa[0].data),
+            np.nanmax(bb[0].data),
+        )
+        diff = aa[0].data[0][0] - bb[0].data[0][0]
+        f, ax = plt.subplots(1, 1)
+        ax.imshow(diff, aspect="auto", origin="lower", vmin=-1.0e-1, vmax=1.0e-1)
         plt.show()
+
 
 if __name__ == "__main__":
     unittest.main()
