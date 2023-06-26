eb635ef4e2a30dcdfb6ff7930f7a3fc58dc80f30
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2023-06-26 14:48:37 +0200

    test-bug-commit

--- karabo/test/test_ionosphere.py
+++ karabo/test/test_ionosphere.py
@@ -75,10 +75,9 @@ class TestSystemNoise(unittest.TestCase):
         speed = 150e3 / 3600.0  # 150 km/h in m/s.
         rate = 1.0 / 60.0  # The inverse frame rate (1 per minute).
         alpha_mag = 0.999  # Evolve screen slowly.
-        num_times = 240  # Four hours.
+        num_times = 1  # one minute (64MB RAM per minute)
         frequency = 1.0e8
         fits_filename = "result/test_screen_60s.fits"
-        print("test_ionosphere-1")
         self.sim_ion(
             screen_width_metres,
             r0,
@@ -91,7 +90,6 @@ class TestSystemNoise(unittest.TestCase):
             frequency,
             fits_filename,
         )
-        print("test_ionosphere-2")
         # ---------- Simulation with Screen
         sky = SkyModel()
         sky_data = np.array(
@@ -104,10 +102,8 @@ class TestSystemNoise(unittest.TestCase):
         sky.add_point_sources(sky_data)
         # sky = SkyModel.get_random_poisson_disk_sky((220, -60), (260, -80), 1, 1, 1)
         # sky.explore_sky([240, -70])
-        print("test_ionosphere-3")
         telescope = Telescope.get_SKA1_LOW_Telescope()
         # telescope.centre_longitude = 3
-        print("test_ionosphere-4")
         simulation = InterferometerSimulation(
             channel_bandwidth_hz=1e6,
             time_average_sec=1,
@@ -129,19 +125,13 @@ class TestSystemNoise(unittest.TestCase):
             frequency_increment_hz=1e6,
             number_of_channels=1,
         )
-        print("test_ionosphere-5")
         visibility = simulation.run_simulation(telescope, sky, observation)
-        print("test_ionosphere-6")
         visibility.write_to_file("./result/test_ion.ms")
-        print("test_ionosphere-7")
         imager = Imager(
             visibility, imaging_npixel=2048 * 1, imaging_cellsize=5.0e-5
         )  # imaging cellsize is over-written in the Imager based on max uv dist.
-        print("test_ionosphere-8")
         dirty = imager.get_dirty_image()
-        print("test_ionosphere-9")
         dirty.write_to_file("result/test_ion.fits", overwrite=True)
-        print("test_ionosphere-10")
         dirty.plot(title="Flux Density (Jy)")
 
 
