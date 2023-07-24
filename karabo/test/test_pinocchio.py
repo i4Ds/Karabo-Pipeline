476a7fbd21f1659410413e899c91daec0938484e
Luis Fernando Machado Poletti Valle <luisfernando.machadopoletti@gmail.com>
2023-07-24 11:29:46 +0200

    Begin improving PINOCCHIO tests

--- karabo/test/test_pinocchio.py
+++ karabo/test/test_pinocchio.py
@@ -1,50 +1,18 @@
-import datetime
 import os
-import tempfile
+from pathlib import Path
 
-from karabo.imaging.imager import Imager
-from karabo.simulation.interferometer import InterferometerSimulation
-from karabo.simulation.observation import Observation
 from karabo.simulation.pinocchio import Pinocchio
-from karabo.simulation.telescope import Telescope
 
 
 def test_simple_instance():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        p = Pinocchio(working_dir=tmpdir)
-        p.setRunName("test")
-        p.printConfig()
-        p.printRedShiftRequest()
-        p.runPlanner(16, 1)
-        p.run(mpiThreads=2)
+    tmpdir = str(Path("/users", "lmachado", "PINOCCHIOOUTPUT"))
+    p = Pinocchio(working_dir=tmpdir)
+    p.setRunName("test")
+    print(p.paramsInputPath)
+    exit()
+    p.printConfig()
+    p.printRedShiftRequest()
+    p.runPlanner(16, 1)
+    p.run(mpiThreads=2)
 
-        p.save(os.path.join(tmpdir, "subdir"))
-        sky = p.getSkyModel()
-        sky = sky.filter_by_radius(0, 1, 32, 45)
-
-        telescope = Telescope.get_SKA1_MID_Telescope()
-
-        simulation = InterferometerSimulation(
-            channel_bandwidth_hz=1e3, time_average_sec=10
-        )
-
-        observation = Observation(
-            start_frequency_hz=1e9,
-            phase_centre_ra_deg=31.9875,
-            phase_centre_dec_deg=45.1333,
-            length=datetime.timedelta(minutes=10),
-            number_of_time_steps=1,
-            frequency_increment_hz=1,
-            number_of_channels=1,
-            start_date_and_time="2022-03-01T11:00:00",
-        )
-
-        visibility = simulation.run_simulation(telescope, sky, observation)
-
-        cellsize = 0.003
-        boxsize = 2048
-        imager = Imager(visibility, imaging_npixel=boxsize, imaging_cellsize=cellsize)
-
-        dirty = imager.get_dirty_image()
-        dirty.write_to_file(os.path.join(tmpdir, "dirty.fits"), overwrite=True)
-        dirty.plot("pinocchio sim dirty plot")
+    p.save(os.path.join(tmpdir, "subdir"))
