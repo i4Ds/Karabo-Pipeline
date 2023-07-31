0b06fcea1c54c6af838c9a989ac2b34dfe7b3de0
Jennifer Studer <jeje.studer@bluewin.ch>
2023-07-31 14:25:17 +0200

    Add mosaic_header function

--- karabo/imaging/mosaic.py
+++ karabo/imaging/mosaic.py
@@ -1,11 +1,12 @@
 import os
 import shutil
 from pathlib import Path
+from typing import Optional
+
+from MontagePy.archive import *  # noqa
+from MontagePy.main import mHdr
 
 from karabo.util._types import FilePathType
-
-# from MontagePy.archive import *
-# from MontagePy.main import *
 
 
 def mosaic_directories(output_directory_path: FilePathType) -> None:
@@ -26,3 +27,48 @@ def mosaic_directories(output_directory_path: FilePathType) -> None:
     os.makedirs(output_directory_path / "raw")
     os.makedirs(output_directory_path / "projected")
     os.makedirs(output_directory_path / "unused_output")
+
+
+def mosaic_header(
+    output_directory_path: FilePathType,
+    location: str,
+    width: float,
+    height: Optional[float] = None,
+    resolution: Optional[float] = 1.0,
+    sin_projection: Optional[bool] = False,
+) -> None:
+    """
+    Creates the header for the mosaic you want to create.
+
+    :param output_directory_path: The folder path in which all subfolders used for the
+                                  mosaic are.
+    :param location: The center of the mosaic in "RA, Dec" in degrees (Equatorial).
+    :param width: Image width in degrees.
+    :param height: Image height in degrees.
+    :param resolution: Image pixel resolution (in arcsec).
+    :param sin_projection: Set to true if you want the output mosaic to be in SIN
+                           projection. The output fits files from a karabo simulation of
+                           a dirty image are in SIN projection.
+    """
+    output_directory_path = Path(output_directory_path)
+
+    if height is None:
+        height = width
+
+    rtn = mHdr(
+        locstr=location,
+        width=width,
+        height=height,
+        outfile=str(output_directory_path / "region.hdr"),
+        resolution=resolution,
+        csys="Equatorial",
+    )
+    print("mHdr:             " + str(rtn), flush=True)
+
+    if sin_projection:
+        with open(output_directory_path / "region.hdr", "r") as file:
+            data = file.readlines()
+        data[5] = "CTYPE1  = 'RA---SIN'\n"
+        data[6] = "CTYPE2  = 'DEC--SIN'\n"
+        with open(output_directory_path / "region.hdr", "w") as file:
+            file.writelines(data)
