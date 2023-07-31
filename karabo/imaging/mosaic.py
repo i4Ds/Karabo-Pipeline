c120da4e5ce48149592d9f9eee80905593000c6a
Jennifer Studer <jeje.studer@bluewin.ch>
2023-07-31 15:25:09 +0200

    Add mosaic function

--- karabo/imaging/mosaic.py
+++ karabo/imaging/mosaic.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Optional
 
 from MontagePy.archive import *  # noqa
-from MontagePy.main import mHdr
+from MontagePy.main import mAdd, mHdr, mImgtbl, mProjExec
 
 from karabo.util._types import FilePathType
 
@@ -72,3 +72,54 @@ def mosaic_header(
         data[6] = "CTYPE2  = 'DEC--SIN'\n"
         with open(output_directory_path / "region.hdr", "w") as file:
             file.writelines(data)
+
+
+def mosaic(
+    output_directory_path: FilePathType,
+    image_directory: Optional[FilePathType] = "raw",
+    projected_directory: Optional[FilePathType] = "projected",
+):
+    """
+    Coadds the images saved in the "raw" folder by using the mean value. This function
+    does not do any corrections.
+
+    :param output_directory_path: The folder path in which all subfolders used for the
+                                  mosaic are.
+    :param image_directory: The name of the subfolder in which the images to be coadded
+                            are saved.
+    :param projected_directory: The name of the subfolder in which the reprojected
+                                images will be stored.
+    """
+    output_directory_path = Path(output_directory_path)
+    image_directory = Path(image_directory)
+
+    # Scan the images for their coverage metadata.
+    rtn = mImgtbl(
+        str(output_directory_path / image_directory),
+        str(output_directory_path / "rimages.tbl"),
+    )
+    print("mImgtbl (raw):    " + str(rtn), flush=True)
+
+    # Reproject the original images to the  frame of the output FITS header we created
+    rtn = mProjExec(
+        str(output_directory_path / image_directory),
+        str(output_directory_path / "rimages.tbl"),
+        str(output_directory_path / "region.hdr"),
+        projdir=str(output_directory_path / projected_directory),
+    )
+    print("mProjExec:           " + str(rtn), flush=True)
+
+    mImgtbl(
+        str(output_directory_path / projected_directory),
+        str(output_directory_path / "pimages.tbl"),
+    )
+    print("mImgtbl (projected): " + str(rtn), flush=True)
+
+    # Coaddition
+    rtn = mAdd(
+        str(output_directory_path / projected_directory),
+        str(output_directory_path / "pimages.tbl"),
+        str(output_directory_path / "region.hdr"),
+        str(output_directory_path / "mosaic_uncorrected.fits"),
+    )
+    print("mAdd:                " + str(rtn), flush=True)
