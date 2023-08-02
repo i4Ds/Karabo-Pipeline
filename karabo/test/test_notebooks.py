3e55e23949f504dd0608b836d21a9b252a76128b
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2023-08-02 10:52:55 +0200

    improved test-notebooks.py :facepunch:

--- karabo/test/test_notebooks.py
+++ karabo/test/test_notebooks.py
@@ -9,15 +9,7 @@ from karabo.util.plotting_util import Font
 RUN_SLOW_TESTS = os.environ.get("RUN_SLOW_TESTS", "false").lower() == "true"
 
 
-# Test preparation moved to fixture
-@pytest.fixture(scope="session", autouse=True)
-def setup_test_environment():
-    path_to_notebooks = os.path.join("karabo", "examples")
-    os.chdir(path_to_notebooks)
-
-
-@pytest.mark.skip(reason="Not a test")
-def _test_notebook(notebook):
+def _run_notebook(notebook: str) -> None:
     print(Font.BOLD + Font.BLUE + "Testing notebook " + notebook + Font.END)
 
     with open(notebook) as f:
@@ -26,19 +18,19 @@ def _test_notebook(notebook):
         try:
             assert ep.preprocess(nb) is not None, f"Got empty notebook for {notebook}"
         except Exception:
-            assert False, f"Failed executing {notebook}"
+            pytest.fail(reason=f"Failed executing {notebook}")
 
 
 @pytest.mark.skipif(not RUN_SLOW_TESTS, reason="RUN_SLOW_TESTS")
-def test_source_detection_notebook():
-    _test_notebook(notebook="source_detection.ipynb")
+def test_source_detection_notebook() -> None:
+    _run_notebook(notebook="source_detection.ipynb")
 
 
 @pytest.mark.skipif(not RUN_SLOW_TESTS, reason="RUN_SLOW_TESTS")
-def test_source_detection_assesment_notebook():
-    _test_notebook(notebook="source_detection_assessment.ipynb")
+def test_source_detection_assesment_notebook() -> None:
+    _run_notebook(notebook="source_detection_assessment.ipynb")
 
 
 @pytest.mark.skipif(not RUN_SLOW_TESTS, reason="SLOW_TESTS")
-def test_HIIM_Img_Recovery_notebook():
-    _test_notebook(notebook="HIIM_Img_Recovery.ipynb")
+def test_HIIM_Img_Recovery_notebook() -> None:
+    _run_notebook(notebook="HIIM_Img_Recovery.ipynb")
