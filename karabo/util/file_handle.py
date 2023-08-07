570e671d3760de752f2122877435a7cf2d01f2a7
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2023-08-07 13:54:07 +0200

    removed FileHandle and added get-file-handler to FileHandler :keycap_ten:

--- karabo/util/file_handle.py
+++ karabo/util/file_handle.py
@@ -6,7 +6,7 @@ import re
 import shutil
 import uuid
 from types import TracebackType
-from typing import Optional
+from typing import Any, Optional
 
 from karabo.util.plotting_util import Font
 
@@ -125,6 +125,24 @@ class FileHandler:
                 else:  # remove fh-root if dir is empty
                     shutil.rmtree(FileHandler.root)
 
+    @staticmethod
+    def get_file_handler(obj: object, *args: Any, **kwargs: Any) -> FileHandler:
+        """Utility function to always get unique `FileHandler` bound to `obj`.
+
+        Args:
+            obj: Any object which should have an unique `FileHandler` assigned.
+
+        Returns:
+            The `FileHandler` bound to `obj`.
+        """
+        for attr_name in obj.__dict__:
+            attr = getattr(obj, attr_name)
+            if isinstance(attr, FileHandler):
+                return attr
+        fh = FileHandler(*args, **kwargs)
+        setattr(obj, "file_handler", fh)
+        return fh
+
     def __enter__(self) -> str:
         return self.subdir
 
@@ -135,126 +153,6 @@ class FileHandler:
         exc_tb: Optional[TracebackType],
     ) -> None:
         self.clean_up()
-
-
-class FileHandle:
-    """
-    Utility class for handling file paths and creating temporary
-    files in a given directory.
-
-    Parameters
-    ----------
-    path : str, optional
-        File path where the resulting file will be stored.
-        If not provided, a uuid will be created.
-        Can also be a folder but then it needs to be a
-        .ms file.
-    dir : str, optional
-        Directory where the resulting file will be stored.
-        If not provided, it either will be saved in the current
-        working directory or in a scratch folder if it exists.
-    create_additional_folder_in_dir : bool, optional
-        Whether to create a new folder inside the directory specified
-        by dir. Default is False. If True, a unique UUID will be used
-        as the name of the folder.
-    file_name : str, optional
-        Name of the output file. If not provided, a unique UUID will
-        be used as the filename.
-    file_is_dir : bool
-        If the file_name is a folder instead of a file.
-        Example: for measurement sets the "file" is a folder.
-    suffix : str, optional
-        Suffix to add to the filename. Default is an empty string.
-        Example: ".MS"
-
-    Attributes
-    ----------
-    path : str
-        Absolute path to the file or directory.
-
-    Methods
-    -------
-    clean_up() -> None:
-        Remove the directory and its contents.
-    remove_file(file_path: str) -> None:
-        Remove a file given its absolute path.
-    copy_file(file_path: str) -> None:
-        Copy a file to the directory.
-    save_file(file_path: str, file_name: str) -> None:
-        Save a file to the directory with a given name.
-
-    """
-
-    def __init__(
-        self,
-        path: Optional[str] = None,
-        dir: Optional[str] = None,
-        create_additional_folder_in_dir: bool = False,
-        file_name: Optional[str] = None,
-        file_is_dir: bool = False,
-        suffix: str = "",
-    ) -> None:
-        use_scratch_folder_if_exist: bool = True
-        use_slurm_job_id_if_exist: bool = True
-
-        # Some logic
-        if not dir:
-            if path:
-                dir = os.path.split(path)[0]
-            elif "SCRATCH" in os.environ and use_scratch_folder_if_exist:
-                dir = os.path.join(os.environ["SCRATCH"], "karabo_folder")
-            else:
-                dir = os.path.join(os.getcwd(), "karabo_folder")
-            if "SLURM_JOB_ID" in os.environ and use_slurm_job_id_if_exist:
-                dir = os.path.join(dir, os.environ["SLURM_JOB_ID"])
-
-        if create_additional_folder_in_dir:
-            dir = os.path.join(dir, str(uuid.uuid4()))
-
-        if not file_name:
-            if path:
-                file_name = path.split(os.path.sep)[-1]
-            else:
-                file_name = str(uuid.uuid4()) + suffix
-
-        if not path:
-            path = os.path.join(dir, file_name)
-
-        # Add some logic to file_is_dir
-        if path.lower().endswith(".ms") or file_name.lower().endswith(".ms"):
-            file_is_dir = True
-
-        if file_is_dir:
-            os.makedirs(path, exist_ok=True)
-        else:
-            os.makedirs(dir, exist_ok=True)
-
-        # Set variables
-        self.path = path
-        self.dir = dir
-        self.file_name = file_name
-
-    def clean_up(self) -> None:
-        if os.path.exists(self.path):
-            if os.path.isfile(self.path):
-                self.remove_file(self.path)
-            else:
-                self.remove_dir(self.path)
-
-    def remove_file(self, file_path: str) -> None:
-        os.remove(file_path)
-
-    def remove_dir(self, dir_path: str) -> None:
-        shutil.rmtree(dir_path)
-
-    def copy_file(self, file_path: str) -> None:
-        file_name = file_path.split(os.path.sep)[-1]
-        shutil.copy(file_path, self.path + os.path.sep + file_name)
-
-    def save_file(self, file_path: str, file_name: Optional[str] = None) -> None:
-        if file_name is None:
-            file_name = file_path.split(os.path.sep)[-1]
-        shutil.copy(file_path, self.path + os.path.sep + file_name)
 
 
 def check_ending(path: str, ending: str) -> None:
