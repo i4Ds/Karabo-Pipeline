c45b68188715b85a621a9b0ec96d35c4fa7fb138
Luis Fernando Machado Poletti Valle <luisfernando.machadopoletti@gmail.com>
2023-10-12 13:15:59 +0200

    Make notebook error more specific

--- karabo/test/test_notebooks.py
+++ karabo/test/test_notebooks.py
@@ -8,9 +8,11 @@ from karabo.test.conftest import IS_GITHUB_RUNNER
 from karabo.util.plotting_util import Font
 
 RUN_NOTEBOOK_TESTS = os.environ.get("RUN_NOTEBOOK_TESTS", "false").lower() == "true"
+print(RUN_NOTEBOOK_TESTS)
 
 # get notebook-dir not matter cwd
 notebook_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "examples")
+print(notebook_dir)
 
 
 def _run_notebook(notebook: str) -> None:
@@ -24,8 +26,10 @@ def _run_notebook(notebook: str) -> None:
         os.chdir(notebook_dir)
         try:
             assert ep.preprocess(nb) is not None, f"Got empty notebook for {notebook}"
-        except Exception:
-            pytest.fail(reason=f"Failed executing {notebook}")
+        except AssertionError as e:
+            pytest.fail(reason=f"Assertion error, details: {e}")
+        except Exception as e:
+            pytest.fail(reason=f"Failed executing {notebook}, Exception: {e}")
         finally:
             os.chdir(cwd)
 
@@ -51,6 +55,7 @@ def test_source_detection_big_files_notebook() -> None:
     reason="'Error: The operation was canceled' when running this test on the package",
 )
 def test_source_detection_assesment_notebook() -> None:
+    print("HEHEHE")
     _run_notebook(notebook="source_detection_assessment.ipynb")
 
 
