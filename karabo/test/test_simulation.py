9081192d47f5c6fe0169aaa97435bb0f3d0f5c26
Luis Fernando Machado Poletti Valle <luisfernando.machadopoletti@gmail.com>
2023-11-09 12:01:33 +0100

    Add pytest-order

--- karabo/test/test_simulation.py
+++ karabo/test/test_simulation.py
@@ -52,6 +52,7 @@ def continuous_noise_fits_downloader(
     )
 
 
+@pytest.mark.order(1)
 def test_parallelization_by_observation() -> None:
     sky = SkyModel.get_GLEAM_Sky([76])
     phase_center = [250, -80]
