a0fcba9e1f4003d3ffff6368381d1616b9ab5316
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2023-11-20 16:22:11 +0100

    adapted exclude-option in setup.cfg to hopefully ignore __init__.py on the runners :pig_nose:

--- karabo/__init__.py
+++ karabo/__init__.py
@@ -29,14 +29,12 @@ if "WSL" in platform.release() and (
 # Setup dask for slurm
 if "SLURM_JOB_ID" in os.environ:
     # ugly workaraound to not import stuff not available at build-time, but on import.
-    from karabo.util.dask import prepare_slurm_nodes_for_dask  # type: ignore
+    from karabo.util.dask import prepare_slurm_nodes_for_dask
 
     prepare_slurm_nodes_for_dask()
 
 # set rascil data directory environment variable
 # see https://ska-telescope.gitlab.io/external/rascil/RASCIL_install.html
-from karabo.util.setup_pkg import (  # type: ignore  # noqa: E402
-    set_rascil_data_directory_env,
-)
+from karabo.util.setup_pkg import set_rascil_data_directory_env  # noqa: E402
 
 set_rascil_data_directory_env()
