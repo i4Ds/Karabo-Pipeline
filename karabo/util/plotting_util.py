aa21e3e4c09aecbcafd40f7746acf075a52f929d
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2024-01-24 13:38:17 +0100

    readded accidentally removed plotting-util.py :sandal:

---
+++ karabo/util/plotting_util.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from astropy.wcs import WCS
+
+
+def get_slices(wcs: WCS) -> List[str]:
+    slices: List[str] = []
+    for i in range(wcs.pixel_n_dim):
+        if i == 0:
+            slices.append("x")
+        elif i == 1:
+            slices.append("y")
+        else:
+            slices.append(0)  # type: ignore
+    return slices
+
+
+class Font:
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[36m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
