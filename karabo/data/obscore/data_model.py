57f61dc313471a50a4b0720fea6a1e344569f556
lukas.gehrig <lukas.gehrig@fhnw.ch>
2024-07-02 16:56:34 +0200

    enhanced docstring and added polarization helper functions :aquarius:

--- karabo/data/obscore/data_model.py
+++ karabo/data/obscore/data_model.py
@@ -8,8 +8,10 @@ from __future__ import annotations
 import json
 from dataclasses import asdict, dataclass
 from pathlib import Path
-from typing import Literal
+from typing import Literal, get_args
 from warnings import warn
+
+from typing_extensions import TypeGuard
 
 from karabo.util.file_handler import assert_valid_ending
 
@@ -24,14 +26,34 @@ _DataProductTypeType = Literal[
     "measurements",
 ]
 _CalibLevelType = Literal[0, 1, 2, 3, 4]
+_PolStatesType = Literal[
+    "I",
+    "Q",
+    "U",
+    "V",
+    "RR",
+    "LL",
+    "RL",
+    "LR",
+    "XX",
+    "YY",
+    "XY",
+    "YX",
+    "POLI",
+    "POLA",
+]
+_PolStatesListType = list[_PolStatesType]
 
 
 @dataclass
 class ObsCoreMeta:
-    """IVOA ObsCore metadata (TAP column names).
+    r"""IVOA ObsCore v1.1 metadata (TAP column names).
 
     This doesn't describe a full ObsCoreDM, but the mandatory and the ones
     defined in the SRCNet rucio database.
+
+    The args-docstring provides just a rough idea of the according values. A
+    more detailed description is provided by the ObsCore-v1.1 documentation.
 
     Args:
         dataproduct_type: Logical data product type (image etc.). `image`, `cube`,
@@ -60,23 +82,35 @@ class ObsCoreMeta:
         access_url: URL used to access (download) dataset.
         access_format: File content format (MIME type) (`fits`, `jpeg`, `zip`, etc.).
         access_estsize: [kbyte] Estimated size of dataset from `access_url`.
-        target_name: Astronomical object observed, if any.
+        target_name: Astronomical object observed, if any. This is typically the name
+            of an astronomical object, but could be the name of a survey field.
         s_ra: [deg] Central right ascension, ICRS.
         s_dec: [deg] Central declination, ICRS.
-        s_fov: [deg] Diameter (bounds) of the covered region.
+        s_fov: [deg] Region covered by the data product. For a circular region, this
+            is the diameter. For most data products, the value should be large enough
+            to include the entire are of the observation. For detailed spatial
+            coverage, the `s_region` attribute can be used.
         s_region: Sky region covered by the data product (expressed in ICRS frame).
-        s_resolution: [arcsec] Spatial resolution of data as FWHM.
+            The format for `point`, `circle` and `polygon` is described in `DALI-1.1`.
+        s_resolution: [arcsec] Smallest resolvable spatial resolution of data as FWHM.
+            If spatial frequency sampling is complex (e.g. interferometry), a typical
+            value for spatial resolution estimate should be given.
         s_xel1: Number of elements along the first spatial axis.
         s_xel2: Number of elements along the second spatial axis.
         s_pixel_scale: Sampling period in world coordinate units along the spatial axis.
-        t_min: [d] Start time in MJD.
-        t_max: [d] Stop time in MJD.
-        t_exptime: [s] Total exposure time.
-        t_resolution: [s] Temporal resolution FWHM.
+        t_min: [d] Observation start time in Modified Julian Day (MJD).
+        t_max: [d] Observation stop time in Modified Julian Day (MJD).
+        t_exptime: [s] Total exposure time. For simple exposures: `t_max` - `t_min`.
+            For data where the exposure is not constant over the entire data product,
+            the median exposure time per pixel is a good wa to characterize the typical
+            value.
+        t_resolution: [s] Minimal interpretable interval between two points along time.
+            This can be an average or representative value. For products with no
+            sampling along the time axis, it could be set to exposure time or null.
         t_xel: Number of elements along the time axis.
-        em_min: [m] Start in spectral coordinates.
-        em_max: [m] Stop in spectral coordinates.
-        em_res_power: Spectral resolving power.
+        em_min: [m] Minimal spectral value observed, expressed as a vacuum wavelength.
+        em_max: [m] Maximum spectral value observed, expressed as a vacuum wavelength.
+        em_res_power: Spectral resolving power :math:`\lambda / \delta \lambda`.
         em_xel: Number of elements along the spectral axis.
         em_ucd: Nature of the spectral axis.
         o_ucd: UCD of observable (e.g. phot.flux.density, phot.count, etc.)
@@ -166,3 +200,38 @@ class ObsCoreMeta:
             }
         with open(file=fpath, mode="w") as json_file:
             json_file.write(json.dumps(dictionary))
+
+    def set_pol_states(self, pol_states: _PolStatesListType) -> None:
+        """Sets `pol_states` from a pythonic interface to a `str` according to ObsCore.
+
+        Args:
+            pol_states: Polarisation states.
+        """
+        all_pol_states = set(get_args(_PolStatesType))
+        pol_states_ordered = all_pol_states - (all_pol_states - set(pol_states))
+        pol_states_str = "/".join(("", *pol_states_ordered, ""))
+        self.pol_states = pol_states_str
+        return None
+
+    def get_pol_states(self) -> _PolStatesListType | None:
+        """Parses the polarisation states to `_PolStatesListType`.
+
+        Returns:
+            List of polarisation states if field-value is not None.
+        """
+
+        def check_pol(pol_states: list[str]) -> TypeGuard[_PolStatesListType]:
+            valid_pol_states = get_args(_PolStatesType)
+            return all(pol_state in valid_pol_states for pol_state in pol_states)
+
+        if self.pol_states is None:
+            return None
+        else:
+            pol_states = self.pol_states.split("/")[1:-1]
+            if check_pol(pol_states=pol_states):
+                return pol_states
+            else:
+                err_msg = (
+                    f"Invalid polarization values encountered in {self.pol_states=}"
+                )
+                raise ValueError(err_msg)
