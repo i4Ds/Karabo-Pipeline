ae119f5b868eee4f76f5641eb3207dea2fb93391
anawas <menkalinan56@gmail.com>
2024-10-29 17:27:36 +0100

    Added comment about which value for Earth radius astropy uses :beetle:

--- karabo/simulation/coordinate_helper.py
+++ karabo/simulation/coordinate_helper.py
@@ -20,6 +20,7 @@ def east_north_to_long_lat(
 
     # https://stackoverflow.com/questions/7477003/calculating-new-longitude-latitude-from-old-n-meters
     r_earth = 6371000
+    # astropy uses 6378100.0 (astropy.constants.R_earth.value)
     new_latitude = lat + (east_relative / r_earth) * (180 / np.pi)
     new_longitude = long + (north_relative / r_earth) * (180 / np.pi) / np.cos(
         long * np.pi / 180
