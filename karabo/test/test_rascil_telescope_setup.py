21198dc4e751f2ab1d6579436c9d660362bbb631
anawas <menkalinan56@gmail.com>
2024-11-01 09:48:04 +0100

    Fixed typo :neutral_face:

--- karabo/test/test_rascil_telescope_setup.py
+++ karabo/test/test_rascil_telescope_setup.py
@@ -6,8 +6,21 @@ from karabo.simulation.telescope import RASCILTelescopes, Telescope
 
 
 @pytest.mark.parametrize("site_name", get_args(RASCILTelescopes))
-def test_set_telescope_name(site_name):
+def test_set_telescope_name_from_oskar_telescope(site_name):
     # create dummy telescope, name will be overriden later
     site: Telescope = Telescope.constructor("EXAMPLE")
+
+    site.name = site_name
+    assert site.name == site_name
+
+
+def test_set_telescope_name():
+    site_name = "ASKAP"
+
+    # create dummy telescope, name will be overriden later
+    site: Telescope = Telescope(0.0, 0.0, 0.0)
+    # __init__() sets name to None
+    assert site.name is None
+
     site.name = site_name
     assert site.name == site_name
