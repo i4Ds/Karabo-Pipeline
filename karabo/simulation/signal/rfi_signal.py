2394a623ccf9156d2af38350b61e64357d98699a
anawas <menkalinan56@gmail.com>
2025-07-02 15:04:10 +0200

    update file :necktie:

--- karabo/simulation/signal/rfi_signal.py
+++ karabo/simulation/signal/rfi_signal.py
@@ -14,7 +14,17 @@ from karabo.util.file_handler import FileHandler
 
 _TABSIM_BINARY = "sim-vis"
 TABSIM_DATA_DIR = os.path.join("data", "tabsim")
-TLE_FILE_CACHE = os.path.join(os.path.expanduser("~"), ".tabsim")
+
+
+"""
+tab-sim expects lists in the 'flow style' format, i.e.
+sat_names: [navstar, sputnik] instead of
+sat_names:
+  - navstar
+  - sputnik
+
+The following lines configure the yaml package to do so.
+"""
 
 
 class FlowStyleList(list):
@@ -54,7 +64,7 @@ class RFISignal:
         self.random_seed: int = 999
         """Random seed. Set it it to a predefined value to get reproducible results."""  # noqa: E501
 
-        """tab_sim diagnostic output
+        """tabsim diagnostic output
             rfi_seps: Plots the separation of the RFI sources from the pointing
                 direction over time. Set to True.
             src_alt: Plots the altitude of the pointing over time. Set to False
@@ -66,21 +76,85 @@ class RFISignal:
         self.src_alt: bool = False
         self.uv_cov: bool = False
 
+        """tabsim output settings
+            zarr_output: Write simulation output as zarr file. Set to true, beacuse
+                sim-vis needs zarr file output.
+            ms_output: Write simulation output as measurement set file.
+                This is the preferred output. Set to true.
+            overwrite_output: Overwrite putput files if they exist. Set to true
+        """
+        self.zarr_output: bool = True
+        self.ms_output: bool = True
+        self.overwrite_output: bool = True
+
         self._observation: ObservationAbstract
         self._telescope: Telescope
+        self._sat_names: list = FlowStyleList(["navstar"])  # adding 1 default satellite
+        self._norad_ids: list = FlowStyleList([])
 
-        self.tmp_dir = FileHandler().get_tmp_dir(
-            prefix="sim-vis-files-",
-            purpose="cache for temporary files used by sim-vis",
+        # sim-vis downloads satellite data from space-tracks service. These file
+        # will be cached here. On Linux, they can be found in
+        # ~/.cache/karabo-LTM-<username>-<unique_id>/tabsim-files-<unique_id>
+        self.cache_dir = FileHandler().get_tmp_dir(
+            prefix="tabsim-files-",
+            purpose="file cache for sim-vis",
+            term="long",
             mkdir=True,
         )
 
-        if not os.path.exists(TLE_FILE_CACHE):
-            os.makedirs(TLE_FILE_CACHE, exist_ok=True)
+        # temporary work directory for sim-vis. We put intermediate scripts and
+        # output files here. On Linux, the location is probably
+        # /tmp/karabo-STM-<username>-<unique_id>/tabsim-files-<unique_id>
+        self.tmp_dir = FileHandler().get_tmp_dir(
+            prefix="tabsim-files-",
+            purpose="working directoryfor sim-vis",
+            term="short",
+            mkdir=True,
+        )
+
+    def __set_basic_properties(
+        self,
+    ) -> Dict[str, Dict[str, Union[str, IntFloat, bool, None]]]:
+        """
+        Sets basic properties for sim-vis. Most important parameters are:
+        - output.zarr: When set to true write out data as zarr file
+        - output.ms: Set to true (default) to get a measurement set
+
+        It produces a lot of data, because we set both 'output.zarr' and
+        'output.ms' to True. Adjust the parameters accordingly if you don't need
+        both files.
+
+        Returns:
+            dict: A dictionary containing the basic properties.
+        """
+        return {
+            "output": {
+                "path": self.tmp_dir,
+                "prefix": self._telescope.name,
+                "overwrite": self.overwrite_output,
+                "zarr": self.zarr_output,
+                "ms": self.ms_output,
+            },
+            "gains": {
+                "G0_mean": self.G0_mean,
+                "G0_std": self.G0_std,
+                "Gt_std_amp": self.Gt_std_amp,
+                "Gt_std_phase": self.Gt_std_phase,
+                "Gt_corr_amp": self.Gt_corr_amp,
+                "Gt_corr_phase": self.Gt_corr_phase,
+                "random_seed": self.random_seed,
+            },
+            "diagnostics": {
+                "rfi_seps": self.rfi_seps,
+                "src_alt": self.src_alt,
+                "uv_cov": self.uv_cov,
+            },
+            "dask": {"max_chunk_MB": 100},
+        }
 
     def __set_observation_properties(
         self,
-    ) -> Dict[str, Dict[str, Union[str, IntFloat, bool]]]:
+    ) -> Dict[str, Dict[str, Union[str, IntFloat, bool, None]]]:
         """Get the properties of the observation.
 
         Returns:
@@ -99,7 +173,7 @@ class RFISignal:
                 "start_freq": float(self._observation.start_frequency_hz),
                 "chan_width": float(self._observation.frequency_increment_hz),
                 "n_freq": self._observation.number_of_channels,
-                "SEFD": 420,
+                "SEFD": 420,  # system equivivalent flux density (system noise) in Jy
                 "auto_corrs": False,
                 "no_w": False,
                 "random_seed": 12345,
@@ -120,7 +194,7 @@ class RFISignal:
             dict: A dictionary containing the properties of the observation.
         """
         stations = self._telescope.stations
-        enu_file_path = os.path.join(self.tmp_dir, "stations.enu.txt")
+        enu_file_path = os.path.join(self.cache_dir, "stations.enu.txt")
         with open(enu_file_path, "w") as enu_file:
             for station in stations:
                 # Write the station name and its ENU coordinates to the file.
@@ -151,13 +225,16 @@ class RFISignal:
 
     def __set_satellite_properties(self, credentials_file: FilePathType) -> Dict:
         path_to_data_dir = os.path.join(get_module_absolute_path(), TABSIM_DATA_DIR)
+        tle_file_cache = os.path.join(self.cache_dir, "tles")
         return {
             "rfi_sources": {
                 "tle_satellite": {
+                    "sat_names": FlowStyleList(self._sat_names),
+                    "norad_ids": FlowStyleList(self._norad_ids),
                     "spacetrack_path": credentials_file,
-                    "tle_dir": TLE_FILE_CACHE,
+                    "tle_dir": tle_file_cache,
                     "norad_spec_model": os.path.join(
-                        path_to_data_dir, "norad_spec_model.txt"
+                        path_to_data_dir, "norad_satellite.rfimodel"
                     ),
                     "power_scale": 1e-2,
                     "max_ang_sep": 30,  # degrees
@@ -177,34 +254,18 @@ class RFISignal:
             credentials_file (FilePathType): The name of the file containing the
                 credentials for the spacetrack service. This file is mandatory.
         """
-        obs_properties = self.__set_observation_properties()
-        tel_properties = self.__set_telescope_properties()
-        sat_properties = self.__set_satellite_properties(credentials_file)
 
         with open(filename, "w") as file:
             file.write("# RFI Signal Properties\n\n")
 
-        properties = {
-            "gains": {
-                "G0_mean": self.G0_mean,
-                "G0_std": self.G0_std,
-                "Gt_std_amp": self.Gt_std_amp,
-                "Gt_std_phase": self.Gt_std_phase,
-                "Gt_corr_amp": self.Gt_corr_amp,
-                "Gt_corr_phase": self.Gt_corr_phase,
-                "random_seed": self.random_seed,
-            },
-            "diagnostics": {
-                "rfi_seps": self.rfi_seps,
-                "src_alt": self.src_alt,
-                "uv_cov": self.uv_cov,
-            },
-            "dask": {"max_chunk_MB": 100},
-        }
+        properties = {}
+        properties.update(self.__set_telescope_properties())
+        properties.update(self.__set_observation_properties())
+        properties.update(self.__set_satellite_properties(credentials_file))
+        properties.update(self.__set_basic_properties())
 
-        properties.update(obs_properties)
-        properties.update(tel_properties)
-        properties.update(sat_properties)
+        # configure yaml to write lists in flow style
+        yaml.add_representer(FlowStyleList, represent_flow_style_list)
 
         with open(filename, "a") as file:
             yaml.dump(properties, file)
@@ -241,7 +302,9 @@ class RFISignal:
                 f"Credentials file '{credentials_filename}' does not exist."
             )
 
-        tmp_property_filename = os.path.join(self.tmp_dir, "sim_target_properties.yaml")
+        tmp_property_filename = os.path.join(
+            self.cache_dir, "sim_target_properties.yaml"
+        )
 
         self._observation = observation
         self._telescope = telescope
