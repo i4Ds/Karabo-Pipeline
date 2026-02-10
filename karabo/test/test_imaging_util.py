d0b1f2ac57314df233e46d01c1abac5dc794fb69
anawas <menkalinan56@gmail.com>
2026-02-10 08:40:36 +0100

    Replaced if conditional with distinct test cases. :de:

--- karabo/test/test_imaging_util.py
+++ karabo/test/test_imaging_util.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 import numpy as np
 import pytest
-from numpy.typing import NDArray
 
 from karabo.imaging.imager_base import DirtyImagerConfig
 from karabo.imaging.imager_oskar import OskarDirtyImager
@@ -12,38 +11,20 @@ from karabo.simulation.interferometer import InterferometerSimulation
 from karabo.simulation.observation import Observation
 from karabo.simulation.sky_model import SkyModel
 from karabo.simulation.telescope import Telescope
-from karabo.simulation.visibility import VisibilityFormat
 from karabo.simulator_backend import SimulatorBackend
 from karabo.test.util import get_compatible_dirty_imager
 
 
-@pytest.mark.parametrize(
-    "backend,telescope_name,visibility_format,combine_across_frequencies",
-    [
-        (SimulatorBackend.OSKAR, "SKA1MID", "OSKAR_VIS", True),
-        (SimulatorBackend.OSKAR, "SKA1MID", "MS", True),
-        (SimulatorBackend.RASCIL, "MID", "MS", False),
-    ],
-)
-def test_get_compatible_dirty_imager(
-    sky_data: NDArray[np.float64],
-    backend: SimulatorBackend,
-    telescope_name: str,
-    visibility_format: VisibilityFormat,
-    combine_across_frequencies: bool,
-) -> None:
+@pytest.fixture
+def sky_model() -> SkyModel:
     sky = SkyModel()
-    sky.add_point_sources(sky_data)
     sky = SkyModel.get_random_poisson_disk_sky((220, -60), (260, -80), 1, 1, 1)
-    sky.explore_sky([240, -70], s=10)
-    telescope = Telescope.constructor(telescope_name, backend=backend)
-    telescope.centre_longitude = 3
+    return sky
 
-    simulation = InterferometerSimulation(
-        channel_bandwidth_hz=1e6,
-        time_average_sec=10,
-    )
-    observation = Observation(
+
+@pytest.fixture
+def observation() -> Observation:
+    return Observation(
         start_frequency_hz=100e6,
         start_date_and_time=datetime(2024, 3, 15, 10, 46, 0),
         phase_centre_ra_deg=240,
@@ -53,42 +34,94 @@ def test_get_compatible_dirty_imager(
         number_of_channels=4,
     )
 
+
+@pytest.fixture
+def simulation() -> InterferometerSimulation:
+    return InterferometerSimulation(
+        channel_bandwidth_hz=1e6,
+        time_average_sec=10,
+    )
+
+
+def test_get_compatible_dirty_imager_should_return_OskarDirtyImager(
+    sky_model: SkyModel,
+    observation: Observation,
+    simulation: InterferometerSimulation,
+) -> None:
+    sim_backend = SimulatorBackend.OSKAR
+    telescope = Telescope.constructor("SKA1MID", backend=sim_backend)
+    telescope.centre_longitude = 3
+
     visibility = simulation.run_simulation(
         telescope,
-        sky,
+        sky_model,
         observation,
-        backend=backend,
-        visibility_format=visibility_format,
+        backend=sim_backend,
+        visibility_format="OSKAR_VIS",
     )
 
     dirty_imager_config = DirtyImagerConfig(
         imaging_npixel=1024,
         imaging_cellsize=3 / 180 * np.pi / 1024,
-        combine_across_frequencies=combine_across_frequencies,
+        combine_across_frequencies=True,
     )
 
     dirty_imager = get_compatible_dirty_imager(visibility, dirty_imager_config)
 
-    # First, let's test if we get the right imager back
-    if (telescope_name, visibility_format, combine_across_frequencies) == (
-        "SKA1MID",
-        "OSKAR_VIS",
-        True,
-    ):
-        assert isinstance(dirty_imager, OskarDirtyImager)
-    if (telescope_name, visibility_format, combine_across_frequencies) == (
-        "SKA1MID",
-        "MS",
-        True,
-    ):
-        assert isinstance(dirty_imager, WscleanDirtyImager)
-    if (telescope_name, visibility_format, combine_across_frequencies) == (
-        "MID",
-        "MS",
-        False,
-    ):
-        assert isinstance(dirty_imager, RascilDirtyImager)
+    assert isinstance(dirty_imager, OskarDirtyImager)
 
-    # Now test if the imager works.
-    dirty_image = dirty_imager.create_dirty_image(visibility)
-    assert dirty_image.data.ndim == 4
+
+def test_get_compatible_dirty_imager_should_return_WscleanDirtyImager(
+    sky_model: SkyModel,
+    observation: Observation,
+    simulation: InterferometerSimulation,
+) -> None:
+    sim_backend = SimulatorBackend.OSKAR
+    telescope = Telescope.constructor("SKA1MID", backend=sim_backend)
+    telescope.centre_longitude = 3
+
+    visibility = simulation.run_simulation(
+        telescope,
+        sky_model,
+        observation,
+        backend=sim_backend,
+        visibility_format="MS",
+    )
+
+    dirty_imager_config = DirtyImagerConfig(
+        imaging_npixel=1024,
+        imaging_cellsize=3 / 180 * np.pi / 1024,
+        combine_across_frequencies=True,
+    )
+
+    dirty_imager = get_compatible_dirty_imager(visibility, dirty_imager_config)
+
+    assert isinstance(dirty_imager, WscleanDirtyImager)
+
+
+def test_get_compatible_dirty_imager_should_return_RascilDirtyImager(
+    sky_model: SkyModel,
+    observation: Observation,
+    simulation: InterferometerSimulation,
+) -> None:
+    sim_backend = SimulatorBackend.RASCIL
+    telescope = Telescope.constructor("MID", backend=sim_backend)
+    telescope.centre_longitude = 3
+
+    visibility = simulation.run_simulation(
+        telescope,
+        sky_model,
+        observation,
+        backend=sim_backend,
+        visibility_format="MS",
+    )
+
+    dirty_imager_config = DirtyImagerConfig(
+        imaging_npixel=1024,
+        imaging_cellsize=3 / 180 * np.pi / 1024,
+        combine_across_frequencies=False,
+    )
+
+    dirty_imager = get_compatible_dirty_imager(visibility, dirty_imager_config)
+
+    assert isinstance(dirty_imager, RascilDirtyImager)
